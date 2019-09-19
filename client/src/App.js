b4d5278fb73acc76d1ddb1b790cd151f32879197
kaijacob18 <antionnedavis@gmail.com>
2019-09-19 18:31:32 -0500

    updated view

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,93 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
+import React, { Component } from "react";
+import Jumbotron from "./components/Jumbotron";
 import Nav from "./components/Nav";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import API from "./utils/API";
+import { RecipeList, RecipeListItem } from "./components/RecipeList";
+import { Container, Row, Col } from "./components/Grid";
 
-function App() {
-  return (
-    <Router>
+class App extends Component {
+  state = {
+    recipes: [],
+    recipeSearch: ""
+  };
+
+  handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
+    event.preventDefault();
+    API.getGoogleBooks(this.state.recipeSearch)
+      .then(res => this.setState({ recipes: res.data.items }))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
       <div>
         <Nav />
-        <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch>
+        <Jumbotron />
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <form>
+                <Container>
+                  <Row>
+                    <Col size="xs-9 sm-10">
+                      <Input
+                        name="recipeSearch"
+                        value={this.state.recipeSearch}
+                        onChange={this.handleInputChange}
+                        placeholder="Search For a Recipe"
+                      />
+                    </Col>
+                    <Col size="xs-3 sm-2">
+                      <Button
+                        onClick={this.handleFormSubmit}
+                        type="success"
+                        className="input-lg"
+                      >
+                        Search
+                      </Button>
+                    </Col>
+                  </Row>
+                </Container>
+              </form>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="xs-12">
+              {!this.state.recipes.length ? (
+                <h1 className="text-center">No Recipes to Display</h1>
+              ) : (
+                <RecipeList>
+                  {this.state.recipes.map(recipe => {
+                    return (
+                      <RecipeListItem
+                        key={recipe.volumeInfo.title}
+                        title={recipe.volumeInfo.title}
+                        href={recipe.volumeInfo.previewLink}
+                        description={recipe.volumeInfo.description}
+                        thumbnail={recipe.volumeInfo.imageLinks.thumbnail}
+                      />
+                    );
+                  })}
+                </RecipeList>
+              )}
+            </Col>
+          </Row>
+        </Container>
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
 export default App;
